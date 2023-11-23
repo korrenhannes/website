4084f4839a4c321577da3d87e0fcef587099b6da
korrenhannes <korrenh@gmail.com>
2023-11-23 15:24:39 +0200

    working!

--- my-website/server/app.js
+++ my-website/server/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config(); // Load environment variables from .env file
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,6 +6,12 @@ const authRoutes = require('./routes/auth');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Log environment variables to ensure they are loaded correctly
+console.log('Environment Variables:');
+console.log('PORT:', process.env.PORT);
+console.log('DB_URI:', process.env.DB_URI);
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
 
 // Use CORS and JSON middleware
 app.use(cors());
@@ -21,7 +27,11 @@ mongoose.connect(process.env.DB_URI, {
   useCreateIndex: true,
 })
 .then(() => console.log('Connected to MongoDB'))
-.catch((error) => console.error('MongoDB connection error:', error));
+.catch((error) => {
+    console.error('MongoDB connection error:', error);
+    // Exit the application if there is a connection error
+    process.exit(1);
+});
 
 // Start the server
 app.listen(port, () => {
--- my-website/server/models/User.js
+++ my-website/server/models/User.js
@@ -1,47 +1,30 @@
-const express = require('express');
-const User = require('../models/User');
-const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const router = express.Router();
-
-router.post('/signup', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const existingUser = await User.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).send('Email already in use');
-    }
-
-    const user = new User({ email, password });
-    await user.save();
-    res.status(201).send('User created successfully');
-  } catch (error) {
-    console.error("Signup error:", error);
-    res.status(400).send(error.message);
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
   }
 });
 
-router.post('/login', async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findOne({ email });
-
-    if (!user || !(await user.comparePassword(password))) {
-      return res.status(401).send('Invalid credentials');
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' }, (err, token) => {
-      if (err) {
-        console.error("JWT Error:", err);
-        return res.status(500).send('Error in token generation');
-      }
-      res.send({ token });
-    });
-  } catch (error) {
-    console.error("Login error:", error);
-    res.status(400).send(error.message);
+// Pre-save hook to hash password before saving it to the database
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) {
+    return next();
   }
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
-module.exports = router;
+// Method to compare provided password with the hashed password in the database
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
