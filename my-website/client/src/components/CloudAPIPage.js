4d1e137e376965927e5dd8bb2d63f6b96503ba2a
korrenhannes <korrenh@gmail.com>
2023-11-23 14:02:50 +0200

    rest

--- my-website/client/src/components/CloudAPIPage.js
+++ my-website/client/src/components/CloudAPIPage.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function CloudAPIPage() {
-  const handleButtonClick = () => {
-    // Logic to connect to the cloud API
-    console.log('Connecting to Cloud API...');
-    // Example: axios.get('https://your-cloud-api.com/data')
+  const [apiData, setApiData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const handleButtonClick = async () => {
+    setIsLoading(true);
+    setError(null);
+
+    try {
+      // Replace with your REST API endpoint
+      const response = await axios.get('https://your-rest-api.com/data');
+      setApiData(response.data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <div>
       <h1>Cloud API Interaction</h1>
-      <button onClick={handleButtonClick}>Connect to Cloud API</button>
+      <button onClick={handleButtonClick} disabled={isLoading}>
+        {isLoading ? 'Loading...' : 'Connect to Cloud API'}
+      </button>
+      {error && <p>Error: {error}</p>}
+      {apiData && <pre>{JSON.stringify(apiData, null, 2)}</pre>}
     </div>
   );
 }
