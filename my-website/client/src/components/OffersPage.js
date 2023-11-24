6049f577027756cb222a8ef5032a8355e74942d7
korrenhannes <korrenh@gmail.com>
2023-11-24 10:32:58 +0200

    whith bug in payment

--- my-website/client/src/components/OffersPage.js
+++ my-website/client/src/components/OffersPage.js
@@ -12,20 +12,43 @@ function OffersPage() {
     <div className="container mt-5">
       <h1 className="text-center">Choose Your Plan</h1>
       <div className="row justify-content-center">
+        
+        {/* Plan A */}
         <div className="col-md-6">
-          <div className="card p-4">
-            <div className="mb-3">
-              <h2>Plan A</h2>
-              <p>Description of Plan A...</p>
-              <button className="btn btn-primary w-100" onClick={() => handleSelectPlan('Plan A')}>Select Plan A</button>
-            </div>
-            <div className="mb-3">
-              <h2>Plan B</h2>
-              <p>Description of Plan B...</p>
-              <button className="btn btn-primary w-100" onClick={() => handleSelectPlan('Plan B')}>Select Plan B</button>
+          <div className="card mb-3" style={{ maxWidth: '540px' }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src="path_to_plan_a_image.jpg" className="img-fluid rounded-start" alt="Plan A" />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Plan A</h5>
+                  <p className="card-text">Description of Plan A...</p>
+                  <button className="btn btn-primary" onClick={() => handleSelectPlan('Plan A')}>Select Plan A</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+
+        {/* Plan B */}
+        <div className="col-md-6">
+          <div className="card" style={{ maxWidth: '540px' }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src="path_to_plan_b_image.jpg" className="img-fluid rounded-start" alt="Plan B" />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Plan B</h5>
+                  <p className="card-text">Description of Plan B...</p>
+                  <button className="btn btn-primary" onClick={() => handleSelectPlan('Plan B')}>Select Plan B</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </div>
   );
--- my-website/client/src/components/PaymentPage.js
+++ my-website/client/src/components/PaymentPage.js
@@ -1,26 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 function PaymentPage() {
   const location = useLocation();
   const [plan, setPlan] = useState('');
 
   useEffect(() => {
-    // Retrieve the selected plan from the state
     if (location.state && location.state.plan) {
       setPlan(location.state.plan);
     }
   }, [location.state]);
 
-  const handlePayment = () => {
-    // Implement payment processing logic
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          description: plan,
+          amount: {
+            // Replace with your plan's price
+            value: "10.00", 
+          },
+        },
+      ],
+    });
+  };
+
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then((details) => {
+      console.log("Payment Successful", details);
+      // Handle post-payment actions such as updating database or redirecting to a success page
+    });
+  };
+
+  const onError = (err) => {
+    console.error("Payment error", err);
+    // Handle errors such as payment declined or network issues
   };
 
   return (
     <div>
       <h1>Complete Your Payment</h1>
       <p>You have selected: {plan}</p>
-      <button onClick={handlePayment}>Proceed to Payment</button>
+      <PayPalScriptProvider options={{ "client-id": "YOUR_CLIENT_ID" }}>
+        <PayPalButtons 
+          createOrder={createOrder} 
+          onApprove={onApprove} 
+          onError={onError} 
+        />
+      </PayPalScriptProvider>
     </div>
   );
 }
