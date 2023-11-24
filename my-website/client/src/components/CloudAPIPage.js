6700e75e9ebba805f409661a609e3832864a5e07
korrenhannes <korrenh@gmail.com>
2023-11-24 21:36:20 +0200

    api works

--- my-website/client/src/components/CloudAPIPage.js
+++ my-website/client/src/components/CloudAPIPage.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useSwipeable } from 'react-swipeable';
 
 function CloudAPIPage() {
-  const [apiData, setApiData] = useState(null);
+  const [apiData, setApiData] = useState([]);
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
@@ -11,15 +13,22 @@ function CloudAPIPage() {
     setError(null);
 
     try {
-      // Replace with your REST API endpoint
       const response = await axios.get('https://your-rest-api.com/data');
-      setApiData(response.data);
+      setApiData(response.data.videos); // Assuming the API sends an object with a 'videos' array
+      setCurrentVideoIndex(0); // Reset to the first video
     } catch (err) {
       setError(err.message);
     } finally {
       setIsLoading(false);
     }
   };
+
+  const handlers = useSwipeable({
+    onSwipedLeft: () => setCurrentVideoIndex(index => Math.min(index + 1, apiData.length - 1)),
+    onSwipedRight: () => setCurrentVideoIndex(index => Math.max(index - 1, 0)),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true
+  });
 
   return (
     <div>
@@ -28,7 +37,17 @@ function CloudAPIPage() {
         {isLoading ? 'Loading...' : 'Connect to Cloud API'}
       </button>
       {error && <p>Error: {error}</p>}
-      {apiData && <pre>{JSON.stringify(apiData, null, 2)}</pre>}
+
+      {apiData.length > 0 && (
+        <div {...handlers} className="video-container">
+          {/* Displaying the current video */}
+          {/* Adjust the video element as needed based on your API data structure */}
+          <video key={apiData[currentVideoIndex].id} controls>
+            <source src={apiData[currentVideoIndex].url} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+      )}
     </div>
   );
 }
