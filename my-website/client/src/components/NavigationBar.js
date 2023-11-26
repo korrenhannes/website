69485fb5c17609960063d67ed9b7513feec3a391
korrenhannes <korrenh@gmail.com>
2023-11-26 22:03:24 +0200

    countdown

--- my-website/client/src/components/NavigationBar.js
+++ my-website/client/src/components/NavigationBar.js
@@ -1,44 +1,77 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate from react-router-dom
+import { Link, useNavigate } from 'react-router-dom';
+
+const Countdown = () => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  const getRandomDuration = () => {
+    const minDays = 3;
+    const maxDays = 10;
+    return Math.floor(Math.random() * (maxDays - minDays + 1)) + minDays;
+  };
+
+  useEffect(() => {
+    setTimeLeft(getRandomDuration() * 24 * 60 * 60 * 1000);
+
+    const interval = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime <= 1000) {
+          return getRandomDuration() * 24 * 60 * 60 * 1000;
+        }
+        return prevTime - 1000;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const days = Math.floor(timeLeft / (24 * 60 * 60 * 1000));
+  const hours = Math.floor((timeLeft % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+  const minutes = Math.floor((timeLeft % (60 * 60 * 1000)) / (60 * 1000));
+  const seconds = Math.floor((timeLeft % (60 * 1000)) / 1000);
+
+  return (
+    <span className="countdown">
+      {`${days}d ${hours}h ${minutes}m ${seconds}s`}
+    </span>
+  );
+};
 
 const NavigationBar = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const navigate = useNavigate(); // Create navigate function
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Check for token in local storage to determine login status
     const token = localStorage.getItem('token');
-    setIsLoggedIn(!!token); // Sets isLoggedIn to true if token exists
+    setIsLoggedIn(!!token);
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem('token'); // Clear the token from local storage
-    setIsLoggedIn(false); // Update isLoggedIn state
-    // Redirect to login page or perform other actions as needed
+    localStorage.removeItem('token');
+    setIsLoggedIn(false);
   };
 
   const navigateToCloudAPI = () => {
-    navigate('/cloud-api'); // Function to navigate to CloudAPIPage
+    navigate('/cloud-api');
   };
 
   return (
     <nav className="navigation-bar">
       <div className="nav-logo">
         <button onClick={navigateToCloudAPI} className="logo-button">
-          {/* Insert your logo image or text here */}
           <img src="/path-to-your-logo.png" alt="Logo" />
         </button>
       </div>
       <div className="nav-links">
-        <a href="/products">Products</a>
-        <a href="/learn" className="hide-on-small">Learn</a>
+        <a href="/products">Products <Countdown /></a>
+        <a href="/HowItWorks" className="how-it-works">Why us</a>
         <a href="/safety" className="hide-on-small">Safety</a>
         <a href="/support">Support</a>
       </div>
       <div className="nav-actions">
         {!isLoggedIn && <a href="/login" className="nav-login">Log in</a>}
         {!isLoggedIn
-          ? <Link to="/signup" className="nav-signup no-underline">Join us</Link>
+          ? <Link to="/signup" className="nav-signup no-underline">Join us!</Link>
           : <button onClick={handleLogout} className="nav-signup no-underline">Logout</button>}
       </div>
     </nav>
