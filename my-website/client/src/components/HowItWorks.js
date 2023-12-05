3bdefa95bf64f771c41a3ffabf28a4f54d24e0b1
korrenhannes <korrenh@gmail.com>
2023-12-05 13:51:28 +0200

    how it works

--- my-website/client/src/components/HowItWorks.js
+++ my-website/client/src/components/HowItWorks.js
@@ -8,25 +8,23 @@ const HowItWorks = () => {
   return (
     <div className="how-it-works-container">
       <NavigationBar />
-      <div className="container text-center my-5">
+      <div className="container my-5">
         <h1>It's time to Cliplt.</h1>
         <p className="lead">“Creating content has never been this easy” - With our simple 3 step process you can transform long videos into short and exciting content.</p>
-        <div className="row">
+        <h2 className="how-does-it-work">How does it work?</h2>
+        <div className="row justify-content-center">
           <div className="col-md-4 step">
-            {/* Update the src attribute to the correct path */}
-            <img src="/Untitled.png" alt="Upload" className="icon" />
+            <img src="/path-to-your-icons/upload-icon.png" alt="Upload" className="icon" />
             <h3>Upload.</h3>
             <p>Choose and upload a video of your choosing with a length of up to 2 hours.</p>
           </div>
           <div className="col-md-4 step">
-            {/* Update the src attribute to the correct path */}
-            <img src="/Untitled.png" alt="Cliplt" className="icon" />
+            <img src="/path-to-your-icons/ai-icon.png" alt="Cliplt" className="icon" />
             <h3>Cliplt.</h3>
             <p>Using our advanced AI algorithm you can clip your video and edit it with 1,000+ options of customization including effects and audio.</p>
           </div>
           <div className="col-md-4 step">
-            {/* Update the src attribute to the correct path */}
-            <img src="/Untitled.png" alt="Share" className="icon" />
+            <img src="/path-to-your-icons/share-icon.png" alt="Share" className="icon" />
             <h3>Share it.</h3>
             <p>After choosing and clipping your videos you can simply share your new favorite edits in any platform and let your followers enjoy high-quality content.</p>
           </div>
