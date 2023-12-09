4c78db69592f849c49416ac5ab693bd14a501c19
korrenhannes <korrenh@gmail.com>
2023-12-09 20:28:11 +0200

    Purge cache

--- my-website/server/app.js
+++ my-website/server/app.js
@@ -47,8 +47,8 @@ app.use(express.json());
 app.use(passport.initialize());
 
 // Using routers
-app.use('../routes/auth', authRoutes);
-app.use('../routes/paypal', paypalRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/paypal', paypalRoutes);
 
 // Passport initialization
 app.use(passport.initialize());
