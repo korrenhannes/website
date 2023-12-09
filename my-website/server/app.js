f2e5e9acaaa8c443c3e58765c504c0dc7b320dc0
korrenhannes <korrenh@gmail.com>
2023-12-09 15:57:28 +0200

    Purge cache

--- my-website/server/app.js
+++ my-website/server/app.js
@@ -14,7 +14,10 @@ const paypalRoutes = require('./routes/paypalserver');
 const passport = require('./passportSetup');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 const server = http.createServer(app);
 
 // Configure CORS for Express
