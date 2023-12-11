cc17c68013a2224029d55ffbfe8c901ecc58d2a3
korrenhannes <korrenh@gmail.com>
2023-12-11 18:26:58 +0200

    cron schedule

--- my-website/server/app.js
+++ my-website/server/app.js
@@ -102,7 +102,7 @@ io.on('connection', (socket) => {
 // });
 
 // Cron job to reset tokens
-cron.schedule('0 0 * * *', async () => {
+cron.schedule('*/2 * * * *', async () => {
   console.log('Running a daily check for token renewal');
   const usersToUpdate = await User.find({ dateOfSubscription: { $type: 'string' } });
 
