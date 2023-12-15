8e59bfebd2704dd570b4cebeedeb18c8bb6b7c3b
korrenhannes <korrenh@gmail.com>
2023-12-15 21:55:11 +0200

    backup

--- my-website/client/src/components/PremiumUserPage.js
+++ my-website/client/src/components/PremiumUserPage.js
@@ -1,116 +1,236 @@
 import React, { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import io from 'socket.io-client';
+import videojs from 'video.js';
+import 'video.js/dist/video-js.css';
+import { apiFlask } from '../api'; // Importing the Axios instance for Flask
+import { jwtDecode } from 'jwt-decode';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
+
+
 import NavigationBar from './NavigationBar';
-import '../styles/FullScreen.css';
+import '../styles/FreeUserPage.css';
 import '../styles/NavigationBar.css';
 
 function FreeUserPage() {
+  const navigate = useNavigate(); // Initialize useNavigate
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [userPaymentPlan, setUserPaymentPlan] = useState('free');
   const [videos, setVideos] = useState([]);
   const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+  const [userEmail, setUserEmail] = useState('');
+  const [userVideosLoaded, setUserVideosLoaded] = useState(false);
+  const videoContainerRef = useRef(null); // Ref for the video container
   const backgroundVideoRef = useRef(null);
-  const navigate = useNavigate();
+  const socket = useRef(null);
+  const playerRef = useRef(null);
 
-  const PEXELS_API_KEY = 'your_pexels_api_key'; // Replace with your Pexels API key
-  const PEXELS_API_URL = 'https://api.pexels.com/videos/popular';
+  useEffect(() => {
+    socket.current = io('http://localhost:3000');
+    socket.current.on('connect', () => console.log('Connected to socket.io server'));
+    return () => {
+      if (socket.current) {
+        socket.current.disconnect();
+      }
+    };
+  }, []);
 
-  const fetchVideos = async () => {
+  useEffect(() => {
+    setTimeout(() => {
+      if (backgroundVideoRef.current && !playerRef.current) {
+        playerRef.current = videojs(backgroundVideoRef.current, {
+          autoplay: true,
+          muted: true,
+          controls: false,
+          fluid: true,
+          loop: false
+        }, () => {
+          console.log('Player is ready');
+          fetchVideosFromGCloud();
+        });
+
+        playerRef.current.on('ended', () => {
+          loadNextVideo();
+        });
+      }
+    }, 0);
+
+    return () => {
+      if (playerRef.current) {
+        playerRef.current.off('ended');
+        playerRef.current.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [backgroundVideoRef]);
+
+  const fetchVideosFromGCloud = async () => {
     setIsLoading(true);
     setError(null);
+  
+    let emailToUse = userEmail;
+  
+    if (!emailToUse) {
+      const token = localStorage.getItem('token');
+      if (token) {
+        try {
+          const decoded = jwtDecode(token);
+          emailToUse = decoded.email;
+          setUserEmail(decoded.email);
+        } catch (error) {
+          console.error("Error decoding token:", error);
+          setError('Authentication error. Please log in again.');
+          setIsLoading(false);
+          return;
+        }
+      }
+    }
+  
+    if (!emailToUse) {
+      setError('User email not found. Redirecting to signup...');
+      setIsLoading(false);
+      navigate('/signup'); // Redirect to signup if no email is found
+      return;
+    }
+  
     try {
-      const randomPage = Math.floor(Math.random() * 10) + 1;
-      const response = await axios.get(PEXELS_API_URL, {
-        headers: {
-          Authorization: PEXELS_API_KEY
-        },
+      const response = await apiFlask.get('/signed-urls', {
         params: {
-          per_page: 3, // Fetch 3 videos instead of 2
-          page: randomPage
+          directory: `${emailToUse}/CurrentRun`
+        },
+        headers: {
+          'User-Email': emailToUse  // Include the User-Email header
         }
       });
-
-      setVideos(response.data.videos.map(video => video.video_files[0].link));
-      setCurrentVideoIndex(0); // Reset to the first video
-      backgroundVideoRef.current.src = response.data.videos[0].video_files[0].link;
+  
+      let signedUrls = response.data.signedUrls;
+      if (!signedUrls || signedUrls.length === 0) {
+        const responseFromUndefined = await apiFlask.get('/signed-urls', {
+          params: {
+            directory: `undefined/`
+          },
+          headers: {
+            'User-Email': emailToUse  // Include the User-Email header
+          }
+        });
+  
+        signedUrls = responseFromUndefined.data.signedUrls;
+  
+        if (!signedUrls || signedUrls.length === 0) {
+          setError('No videos found in Google Cloud Storage.');
+          setIsLoading(false);
+          return;
+        }
+      }
+  
+      setVideos(signedUrls);
+      loadVideo(signedUrls[0]);
+      setUserVideosLoaded(true);
     } catch (err) {
-      setError('Error fetching videos from Pexels: ' + err.message);
+      setError(`Error fetching videos: ${err.message}`);
     } finally {
       setIsLoading(false);
     }
   };
+  
 
-  const fetchUserPaymentPlan = async () => {
-    try {
-      const token = localStorage.getItem('token');
-      if (!token) {
-        console.log('No token found, defaulting to free plan');
-        setUserPaymentPlan('free');
-        return;
-      }
-
-      const response = await axios.get('http://localhost:3000/api/user/payment-plan', {
-        headers: {
-          'Authorization': `Bearer ${token}`
-        }
+  
+  
+  
+  const loadVideo = (videoUrl) => {
+    console.log("Loading video URL:", videoUrl); // Log the URL being loaded
+    playerRef.current.src({ src: videoUrl, type: 'video/mp4' });
+    playerRef.current.load();
+    const playPromise = playerRef.current.play();
+  
+    if (playPromise !== undefined) {
+      playPromise.then(() => {
+        // Automatic playback started successfully.
+      }).catch(error => {
+        console.error('Error attempting to play video:', error);
+        // Handle the error for autoplay restrictions.
+        // For example, show a play button to the user to start playback manually.
       });
+    }
+  };
+  
 
-      if (response && response.data && response.data.paymentPlan) {
-        setUserPaymentPlan(response.data.paymentPlan);
-      } else {
-        console.log('No payment plan info found, defaulting to free plan');
-        setUserPaymentPlan('free');
-      }
-    } catch (error) {
-      console.error('Error fetching user payment plan:', error.message);
-      setUserPaymentPlan('free');
+  const loadNextVideo = async () => {
+    let nextIndex = currentVideoIndex + 1;
+
+    if (nextIndex >= videos.length) {
+      nextIndex = 0;
+      setCurrentVideoIndex(nextIndex);
+      loadVideo(videos[nextIndex]);
+    } else {
+      setCurrentVideoIndex(nextIndex);
+      loadVideo(videos[nextIndex]);
     }
   };
 
+  
 
-  useEffect(() => {
-    fetchVideos();
-    fetchUserPaymentPlan();
-
-    // Double click event listener for fetching new video
-    const handleDoubleClick = () => {
-      const nextVideoIndex = (currentVideoIndex + 1) % videos.length;
-      setCurrentVideoIndex(nextVideoIndex);
-      if (videos[nextVideoIndex]) {
-        backgroundVideoRef.current.src = videos[nextVideoIndex];
+  const handleKeyPress = (event) => {
+    if (event.keyCode === 13) { // 13 is the keycode for the Enter key
+      if (!userVideosLoaded) {
+        fetchVideosFromGCloud();
+      } else {
+        loadNextVideo();
       }
-    };
+    }
+};
 
-    window.addEventListener('dblclick', handleDoubleClick);
+useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
 
     return () => {
-      window.removeEventListener('dblclick', handleDoubleClick);
+      window.removeEventListener('keydown', handleKeyPress);
     };
-  }, [currentVideoIndex, videos]);
+}, [currentVideoIndex, videos, userVideosLoaded]);
 
-  const handleRedirection = () => {
-    // Redirect based on payment plan
-    switch(userPaymentPlan) {
-      case 'regular':
-        navigate('/regular-user');
-        break;
-      case 'premium':
-        navigate('/premium-user');
-        break;
-      default:
-        navigate('/free-user'); // Default or non-registered users
+  const handleDownloadVideo = async () => {
+    if (!playerRef.current) {
+      console.error("No video player found");
+      return;
+    }
+    const currentVideoUrl = playerRef.current.currentSrc();
+    if (!currentVideoUrl) {
+      console.error("No video is currently being played");
+      return;
+    }
+    try {
+      const response = await fetch(currentVideoUrl);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const videoBlob = await response.blob();
+      const localUrl = window.URL.createObjectURL(videoBlob);
+      const a = document.createElement('a');
+      a.href = localUrl;
+      a.download = currentVideoUrl.split('/').pop();
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      window.URL.revokeObjectURL(localUrl);
+    } catch (error) {
+      console.error("Error downloading video:", error);
     }
   };
+
 
   return (
     <div className="full-screen-container">
       <NavigationBar />
-      <video ref={backgroundVideoRef} autoPlay muted loop id="background-video"></video>
-      <div className="foreground-content">
-        {/* Removed <h1> and <button> elements */}
+
+      <div className="video-container" ref={videoContainerRef}>
+        <video ref={backgroundVideoRef} className="video-js vjs-big-play-centered" id="background-video"></video>
       </div>
+
+      <div className="video-ui-overlay">
+        {/* Elements for video title, user interaction, etc. */}
+      </div>
+
+      <button onClick={handleDownloadVideo} className="download-button">Download Video</button>
+
       {isLoading && <p>Loading...</p>}
       {error && <p>Error: {error}</p>}
     </div>
