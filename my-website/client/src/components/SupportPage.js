8043e29f1628bd03b3a57946e06153ed43012bdc
jlautman1 <31826588+jlautman1@users.noreply.github.com>
2023-12-15 18:23:04 +0200

    support not finished

--- my-website/client/src/components/SupportPage.js
+++ my-website/client/src/components/SupportPage.js
@@ -1,74 +1,42 @@
-import React, { useState, useEffect, useRef } from 'react';
-import io from 'socket.io-client';
-import videojs from 'video.js';
-import 'video.js/dist/video-js.css';
-import { apiFlask } from '../api'; // Importing the Axios instance for Flask
-import { useNavigate } from 'react-router-dom';
-import NavigationBar from './NavigationBar';
-import CaptionOptions from './CaptionOptions';
+import React from 'react';
+import NavigationBar from './NavigationBar'; // Assuming you have a navigation bar component
 import '../styles/FullScreen.css';
 import '../styles/NavigationBar.css';
-import '../styles/Sidebar.css';
+import '../styles/Support.css'; // New CSS file for support page
+import CloudAPIPage from './CloudAPIPage';
 
 function SupportPage() {
-    const navigate = useNavigate();
-    const touchStartRef = useRef(0);
-    const handleSwipe = (direction) => {
-      // Placeholder functions - replace these with actual navigation logic
-      const navigateDown = () => navigate('/how-it-works'); // Navigate to your previous page
-      if (direction === 'down') navigateDown();
-    };
-    useEffect(() => {
-      const handleTouchStart = (e) => {
-        touchStartRef.current = e.touches[0].clientY;
-      };
-  
-      const handleTouchMove = (e) => {
-        if (!touchStartRef.current) return;
-        const touchEndY = e.touches[0].clientY;
-        if (touchStartRef.current > touchEndY + 50) {
-          handleSwipe('up');
-        } else if (touchStartRef.current < touchEndY - 50) {
-          handleSwipe('down');
-        }
-      };
-  
-      const handleWheel = (e) => {
-        if (e.deltaY > 100) {
-          handleSwipe('up');
-        } else if (e.deltaY < -100) {
-          handleSwipe('down');
-        }
-      };
-  
-      window.addEventListener('touchstart', handleTouchStart);
-      window.addEventListener('touchmove', handleTouchMove);
-      window.addEventListener('wheel', handleWheel);
-  
-      return () => {
-        window.removeEventListener('touchstart', handleTouchStart);
-        window.removeEventListener('touchmove', handleTouchMove);
-        window.removeEventListener('wheel', handleWheel);
-      };
-    }, []);
 
-    return (
-      <div className="full-screen-container">
-        <div className="sidebar">
-          {/* Populate with actual links and category names */}
-          <h3>Company</h3>
-          <a href="/blog">Blog</a>
-          {/* ... other links */}
-          <h3>Best Practices</h3>
-          <a href="/how-to-videos">How to Turn Long Videos into Viral Shorts</a>
-          {/* ... other links */}
+  return (
+    <div className="full-screen-container-support">
+      <footer className="top-half">
+        <CloudAPIPage />
+      </footer>
+      <footer className="support-footer">
+        <div className="footer-content">
+          <div className="footer-section company-info">
+            <h4>Company (put info and contact)</h4>
+            <ul>
+              <li><a href="/our-story">Our story</a></li>
+              <li><a href="/affiliate">Become an affiliate</a></li>
+              <li><a href="/media-assets">clipIt media assets</a></li>
+              <li><a href="/contact">Contact us</a></li>
+            </ul>
+          </div>
+          
+          <div className="footer-section best-practices">
+            <h4>our buttons (put all buttons from nav bar)</h4>
+            <ul>
+              <li><a href="/guide/turn-videos-into-shorts">How to Turn Long Videos into Viral Shorts</a></li>
+              <li><a href="/guide/create-videos">Create Videos in Alex Hormozi Style</a></li>
+              <li><a href="/guide/how-opusclip-works">How Does OpusClip Work</a></li>
+            </ul>
+          </div>
         </div>
-        <div className="content">
-          <h1>this is the support of <span className="text-primary">ClipIt</span>.</h1>
-          {/* The rest of your content */}
-        </div>
-      </div>
-    );
+      </footer>
+    </div>
+  );
 }
 
 export default SupportPage;
+
