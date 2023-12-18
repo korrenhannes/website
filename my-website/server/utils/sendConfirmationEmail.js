cb45d9f338335195fca0c5c288dc12767d26e37d
korrenhannes <korrenh@gmail.com>
2023-12-18 16:54:38 +0200

    updated mail

--- my-website/server/utils/sendConfirmationEmail.js
+++ my-website/server/utils/sendConfirmationEmail.js
@@ -2,20 +2,32 @@ const nodemailer = require('nodemailer');
 
 async function sendConfirmationEmail(email, confirmationCode) {
   const transporter = nodemailer.createTransport({
-    service: 'Gmail', // Use the email service you prefer
+    service: 'Gmail',
     auth: {
-      user: process.env.EMAIL_USERNAME, // Your email address
-      pass: process.env.EMAIL_PASSWORD, // Your email password
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASSWORD,
     },
   });
 
-  const confirmationUrl = `http://localhost:3000/confirm/${confirmationCode}`; // Replace with your actual frontend route
+  const confirmationUrl = `http://localhost:3000/confirm/${confirmationCode}`;
   const mailOptions = {
-    from: process.env.EMAIL_USERNAME, // Sender's email address
-    to: email, // Recipient's email address
-    subject: 'Confirmation Email',
-    text: `Please confirm your email address by clicking on this link: ${confirmationUrl}`,
-    html: `<p>Please confirm your email address by clicking on the link below:</p><a href="${confirmationUrl}">${confirmationUrl}</a>`, // HTML body
+    from: process.env.EMAIL_USERNAME,
+    to: email,
+    subject: 'Confirm Your Email',
+    html: `
+      <div style="font-family: Arial, sans-serif; color: #333;">
+        <h2 style="color: #1a1a1a;">Welcome to OurApp!</h2>
+        <p>Hi there,</p>
+        <p>We are excited to have you on board. To get started with OurApp, please confirm your email address. This ensures that we have your correct email address and can contact you with important information about your account and our services.</p>
+        <a href="${confirmationUrl}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-align: center; text-decoration: none; display: inline-block; margin-top: 10px; border-radius: 5px;">Confirm Email</a>
+        <p>If you did not sign up for an OurApp account, please ignore this email or <a href="#">contact support</a> if you have any concerns.</p>
+        <footer>
+          <p>Thank you for choosing OurApp!</p>
+          <p>If the button above does not work, copy and paste the following link into your browser:</p>
+          <a href="${confirmationUrl}" style="color: #4CAF50;">${confirmationUrl}</a>
+        </footer>
+      </div>
+    `,
   };
 
   try {
