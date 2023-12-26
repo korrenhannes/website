5c1ddfa448662ed49444846aafa1df9d60543c9d
korrenhannes <korrenh@gmail.com>
2023-12-26 14:53:57 +0200

    a

--- my-website/client/src/components/CloudAPIPage.js
+++ my-website/client/src/components/CloudAPIPage.js
@@ -231,7 +231,7 @@ function CloudAPIPage({ enableScrollHandling = true }) {
 
 
       <div className={styles['foreground-content']}>
-        <h1>Get all the content you will ever need in an instant.</h1>
+        <h1>Never Have Problems Creating Viral Content Again.</h1>
         <div className={styles['search-container']}>
           <form onSubmit={handleSearchSubmit} className={styles['search-form']}>
             <div className={styles['input-logo-container']}>
