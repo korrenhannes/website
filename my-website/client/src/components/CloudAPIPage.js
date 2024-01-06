02438e79c97e5e2e7ee03cc33dfc9f59ddc03385
korrenhannes <134727678+korrenhannes@users.noreply.github.com>
2024-01-06 19:55:56 +0200

    a

--- my-website/client/src/components/CloudAPIPage.js
+++ my-website/client/src/components/CloudAPIPage.js
@@ -58,8 +58,8 @@ function CloudAPIPage({ backgroundImageLoaded, enableScrollHandling = true }) {
   return (
     <div className={styles['full-screen-container']}>
       <Helmet>
-        <title>Cloud API - ClipIt</title> {/* Set the appropriate title */}
-        <link rel="canonical" href="https://www.cliplt.com/home" /> {/* Set the appropriate canonical URL */}
+        <title>ClipIt AI - Short Social Media Content Editor</title> {/* Set the appropriate title */}
+        <link rel="canonical" href="https://www.cliplt.com" /> {/* Set the appropriate canonical URL */}
       </Helmet>
       {backgroundImageLoaded && (
         <LazyLoadImage
