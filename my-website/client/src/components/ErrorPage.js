6724aa84f067163a2af279ae341654687bf390df
korrenhannes <134727678+korrenhannes@users.noreply.github.com>
2024-01-25 20:45:26 +0200

    a

--- my-website/client/src/components/ErrorPage.js
+++ my-website/client/src/components/ErrorPage.js
@@ -11,22 +11,24 @@ const ErrorPage = () => {
   };
 
   return (
-    <div className="error-container">
-      <Helmet>
-        <title>404 Not Found - Revolutionize Your Social Media with ClipIt Shorts AI</title>
-        <meta name="description" content="Encountered a 404 error at ClipIt? Discover how our AI-powered tool is changing the game in social media content creation." />
-        <link rel="canonical" href="https://www.cliplt.com/404" /> 
-        <meta name="robots" content="noindex, follow" /> 
-      </Helmet>
-      
-      <h1>404 - ClipIt Shorts AI Page Not Found</h1>
-      <p>Oops! The page you are looking for does not exist. It might have been moved or deleted. But don't worry, we've got something interesting for you!</p>
-      <button onClick={handleGoBack} className="go-back-btn">
-        Go Back to Home
-      </button>
+    <div className="error-page-container">
+      <div className="error-container">
+        <Helmet>
+          <title>404 Not Found - Revolutionize Your Social Media with ClipIt Shorts AI</title>
+          <meta name="description" content="Encountered a 404 error at ClipIt? Discover how our AI-powered tool is changing the game in social media content creation." />
+          <link rel="canonical" href="https://www.cliplt.com/404" /> 
+          <meta name="robots" content="noindex, follow" /> 
+        </Helmet>
+        
+        <h1>404 - ClipIt Shorts AI Page Not Found</h1>
+        <p>Oops! The page you are looking for does not exist. It might have been moved or deleted. But don't worry, we've got something interesting for you!</p>
+        <button onClick={handleGoBack} className="go-back-btn">
+          Go Back to Home
+        </button>
 
-      <div className="blog-section">
-        <p>Don't let a 404 dampen your spirits. Instead, embrace the opportunity to revolutionize your content creation process with ClipIt. Visit our website to learn more and become a part of our beta program. Together, let's set new standards in social media content creation.</p>
+        <div className="blog-section1">
+          <p>Don't let a 404 dampen your spirits. Instead, embrace the opportunity to revolutionize your content creation process with ClipIt. Visit our website to learn more and become a part of our beta program. Together, let's set new standards in social media content creation.</p>
+        </div>
       </div>
     </div>
   );
